e1b4935fb3c86b0ccb99bd3293feaf2e506d3142
Davy'd <pdaswift3009@gmail.com>
2017-06-18 13:30:02 +0100

    connect-flash add validator  handle session middleware

--- app.js
+++ app.js
@@ -22,13 +22,56 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Handles file upload
+app.use(multer({ dest: './public/images/uploads' }));
+// app.use(multer({ dest: __dirname + '/uploads/' }).any());
+
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Handles express sessions
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+//Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Connect Flash
+app.use(flash());
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Make our db  accessible to our router
+app.use(function (req, res, next ){
+  req.db = db;
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
