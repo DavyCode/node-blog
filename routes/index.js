1c5928d6c6424eb1853c29f9372bef93b8c9f7d9
Davy'd <pdaswift3009@gmail.com>
2017-06-18 16:46:58 +0100

    render post to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.find({}, {}, function(err, posts) {
+    // if(err){
+    //   res.send( "post not found");
+    //   console.log( err.message);
+    // }
+    res.render('index', { "posts" : posts});
+    console.log( posts.body);
+    console.log( posts.title);
+    console.log( posts.author)
+  }); 
 });
 
 module.exports = router;
